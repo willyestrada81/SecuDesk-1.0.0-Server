edde23f9a318360a64715419c66e46247f1223ed
William Estrada <williamestrada.dev@gmail.com>
2021-02-16 01:45:26 -0500

    fixed env connections vars

--- graphql/resolvers/employees.js
+++ graphql/resolvers/employees.js
@@ -7,7 +7,7 @@ const {
   validateRegisterInput,
   validateLoginInput,
 } = require("../../util/validators");
-const { SECRET_KEY } = require("../../config");
+const SECRET_KEY = process.env.SECRET_KEY;
 const Employee = require("../../models/Employees");
 
 function generateToken(employee) {
